d1a7ddd6ffc3228bdc1cf124d35da75d0e4498ee
dmitruz <dmitruz970@gmail.com>
2022-09-13 22:47:36 +0300

    6 finished

--- react-native-hw/screens/nestedScreen/CommentsScreen.js
+++ react-native-hw/screens/nestedScreen/CommentsScreen.js
@@ -1,36 +1,71 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, SafeAreaView, FlatList } from "react-native";
 import { useSelector } from "react-redux";
 import db from "../../firebase/config";
 
 const ComentsScreen = ({ route }) => {
 const { postId } = route.params;
 const [comment, setComment] = useState("");
-const { nickName } = useSelector((state) => {
+const [allComments, setAllComments] = useState("");
+const { nickName } = useSelector((state) => state.auth);
+
+useEffect(() => {
+  getAllPosts();
+}, []);
+
     const createPost = async () => {
         db.firestore()
+        .collection("posts")
+        .doc(postId)
         .collection("comments")
         .add({ comment, nickName });
     };
-})
-    return (
-        <View style={styles.container}>
-            <View style={styles.inputContainer}>
-                <TextInput style={styles.input} onChangeText={setComment} />
-            </View>
-            <TouchableOpacity onPress={createPost} style={styles.sendBtn}>
-                <Text style={styles.sendLabel}>add post</Text>
-            </TouchableOpacity>
-           
-        </View>
 
-    );
+     const getAllPosts = async () => {
+        db.firestore()
+        .collection("posts")
+        .add({ comment, nickName })
+        .collection("comments")
+        .onSnapshot((data) => 
+        setAllComments(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        )
+    };
+
+
+   return (
+    <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={allComments}
+          renderItem={({ item }) => (
+            <View style={styles.commentContainer}>
+              <Text>{item.nickName}</Text>
+              <Text>{item.comment}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} onChangeText={setComment} />
+      </View>
+      <TouchableOpacity onPress={createPost} style={styles.sendBtn}>
+        <Text style={styles.sendLabel}>add post</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-end",
+  },
+  commentContainer: {
+    borderWidth: 1,
+    borderColor: "#20b2aa",
+    marginHorizontal: 10,
+    padding: 10,
+    marginBottom: 10,
   },
   sendBtn: {
     marginHorizontal: 30,
